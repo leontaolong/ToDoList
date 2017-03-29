554f75c5d8b7adb89d48b3a0bd127f562758dc0d
leontaolong <longt8@uw.edu>
2017-03-28 19:29:38 -0700

    First lecture

---
+++ ToDoList/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  ToDoList
+//
+//  Created by LEON LOONG on 3/28/17.
+//  Copyright © 2017 leontaolong. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
